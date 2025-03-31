a7729aa4ec35527323f3c9ca1149eb652bb1da86
Gridlock242 <Neltharion@live.fr>
2025-03-31 12:58:16 +0200

    Modification du header, changement du nom du css qui buguait, ajout d'une barre de recherche à droite

--- templates/header.php
+++ templates/header.php
@@ -1,15 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link href="styles/main.css" rel="stylesheet">
+    <link href="styles/mainStyle.css" rel="stylesheet">
     <title><?= $title ?? "Pokédex" ?></title>
 </head>
+
 <body>
-    <header>
-        <img src="styles/images/pokeball.png" alt="Pokeball">
-        <h1>Projet Pokédex</h1>
+
+    <header class="navbar">
+        <div class="leftLinks">
+            <img src="styles/images/pokeball.png" alt="">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+        </div>
+        <form class="searchForm">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+
     </header>
-    
